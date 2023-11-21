d3ea8fc542d891439493e7369707cccd5e5e3411
annng1510 <105244102+annng1510@users.noreply.github.com>
2023-11-22 03:09:04 +0900

    Update create-table.sql

--- node-api/mysql/create-table.sql
+++ node-api/mysql/create-table.sql
@@ -1,92 +1,50 @@
-
 USE NAD_test;
-
--- Create Fabric_information table
-CREATE TABLE Fabric_information (
-    fabric_code VARCHAR(20) PRIMARY KEY,
-    total_tests INT,
-    defects_detected INT,
-    scan_start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    test_end_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    image_path VARCHAR(100),
-    status ENUM('running', 'complete') DEFAULT 'running'
-);
-
--- Create Defect_information table
-CREATE TABLE Defect_information (
-    defect_code VARCHAR(20) PRIMARY KEY,
-    detection_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    x_coordinate FLOAT,
-    y_coordinate FLOAT,
-    defect_type VARCHAR(50),
-    image_path VARCHAR(100)
+CREATE TABLE NAD_test.fabric_information (
+    fabric_id INT PRIMARY KEY,
+    scan_start_time TIMESTAMP,
+    complete_time TIMESTAMP NULL,
+    defect_count INT,
+    total_count INT,
+    image_path VARCHAR(255)
 );
 
 
-ALTER TABLE Fabric_information
-ADD COLUMN total_test_time INT;
+CREATE TABLE NAD_test.defect_information (
+    defect_code VARCHAR(14) PRIMARY KEY,
+    parent_fabric INT,
+    timestamp TIMESTAMP,
+    issue_name ENUM('hole', 'stain', 'puckering'),
+    x INT,
+    y INT,
+    image_path VARCHAR(255),
+    FOREIGN KEY (parent_fabric) REFERENCES NAD_test.fabric_information(fabric_id)
+);
 
-ALTER TABLE Fabric_information
-ADD COLUMN performance FLOAT;
+INSERT INTO NAD_test.fabric_information (fabric_id, scan_start_time, complete_time, defect_count, total_count, image_path) VALUES
+(1, '2023-11-21 08:00:00', '2023-11-21 08:30:00', 2, 5, '/images/fabric1.jpg'),
+(2, '2023-11-21 09:00:00', '2023-11-21 09:25:00', 1, 6, '/images/fabric2.jpg'),
+(3, '2023-11-21 10:00:00', NULL, 0, 7, '/images/fabric3.jpg'),
+(4, '2023-11-21 11:00:00', '2023-11-21 11:35:00', 3, 8, '/images/fabric4.jpg'),
+(5, '2023-11-21 12:00:00', NULL, 0, 9, '/images/fabric5.jpg'),
+(6, '2023-11-21 13:00:00', '2023-11-21 13:20:00', 2, 10, '/images/fabric6.jpg'),
+(7, '2023-11-21 14:00:00', NULL, 0, 11, '/images/fabric7.jpg'),
+(8, '2023-11-21 15:00:00', '2023-11-21 15:45:00', 4, 12, '/images/fabric8.jpg'),
+(9, '2023-11-21 16:00:00', NULL, 0, 13, '/images/fabric9.jpg'),
+(10, '2023-11-21 17:00:00', '2023-11-21 17:30:00', 1, 14, '/images/fabric10.jpg');
 
-UPDATE Fabric_information
-SET performance = (defects_detected / total_tests) * 100;
+ALTER TABLE NAD_test.defect_information
+MODIFY COLUMN defect_code VARCHAR(50); -- Change 50 to the desired length
 
 
-
-INSERT INTO Fabric_information (fabric_code, total_tests, defects_detected, test_end_time, image_path, status, total_test_time, performance)
+INSERT INTO NAD_test.defect_information (defect_code, parent_fabric, timestamp, issue_name, x, y, image_path)
 VALUES
-  ('Fabric001', 100, 5, '2023-11-14 08:30:00', '/path/to/image1.jpg', 'complete', 3600, 5),
-  ('Fabric002', 150, 10, '2023-11-14 09:45:00', '/path/to/image2.jpg', 'complete', 4500, 6.67),
-  ('Fabric003', 200, 20, '2023-11-14 10:30:00', '/path/to/image3.jpg', 'complete', 5400, 10),
-  ('Fabric004', 120, 8, '2023-11-14 11:15:00', '/path/to/image4.jpg', 'complete', 4320, 6.67),
-  ('Fabric005', 180, 15, '2023-11-14 12:00:00', '/path/to/image5.jpg', 'complete', 6480, 8.33),
-  ('Fabric006', 90, 7, '2023-11-14 13:30:00', '/path/to/image6.jpg', 'complete', 3240, 7.78),
-  ('Fabric007', 110, 9, '2023-11-14 14:15:00', '/path/to/image7.jpg', 'complete', 3960, 8.18),
-  ('Fabric008', 160, 12, '2023-11-14 15:00:00', '/path/to/image8.jpg', 'complete', 5760, 7.5),
-  ('Fabric009', 130, 11, '2023-11-14 16:00:00', '/path/to/image9.jpg', 'complete', 4680, 8.46),
-  ('Fabric010', 140, 14, '2023-11-14 17:00:00', '/path/to/image10.jpg', 'complete', 5040, 10),
-  ('Fabric011', 100, 6, '2023-11-14 18:00:00', '/path/to/image11.jpg', 'complete', 3600, 6),
-  ('Fabric012', 200, 25, '2023-11-14 19:00:00', '/path/to/image12.jpg', 'complete', 7200, 12.5),
-  ('Fabric013', 150, 18, '2023-11-14 20:00:00', '/path/to/image13.jpg', 'complete', 5400, 12),
-  ('Fabric014', 180, 22, '2023-11-14 21:00:00', '/path/to/image14.jpg', 'complete', 6480, 12.22),
-  ('Fabric015', 110, 8, '2023-11-14 22:00:00', '/path/to/image15.jpg', 'complete', 3960, 7.27);
-
-
-  INSERT INTO Fabric_information (fabric_code, total_tests, defects_detected, test_end_time, image_path, status, total_test_time, performance)
-VALUES
-  ('Fabric016', 80, 0, NULL, '/path/to/image16.jpg', 'running', NULL, NULL),
-  ('Fabric017', 95, 0, NULL, '/path/to/image17.jpg', 'running', NULL, NULL),
-  ('Fabric018', 110, 0, NULL, '/path/to/image18.jpg', 'running', NULL, NULL),
-  ('Fabric019', 75, 0, NULL, '/path/to/image19.jpg', 'running', NULL, NULL),
-  ('Fabric020', 105, 0, NULL, '/path/to/image20.jpg', 'running', NULL, NULL),
-  ('Fabric021', 70, 0, NULL, '/path/to/image21.jpg', 'running', NULL, NULL),
-  ('Fabric022', 90, 0, NULL, '/path/to/image22.jpg', 'running', NULL, NULL),
-  ('Fabric023', 100, 0, NULL, '/path/to/image23.jpg', 'running', NULL, NULL),
-  ('Fabric024', 85, 0, NULL, '/path/to/image24.jpg', 'running', NULL, NULL),
-  ('Fabric025', 120, 0, NULL, '/path/to/image25.jpg', 'running', NULL, NULL),
-  ('Fabric026', 78, 0, NULL, '/path/to/image26.jpg', 'running', NULL, NULL),
-  ('Fabric027', 92, 0, NULL, '/path/to/image27.jpg', 'running', NULL, NULL),
-  ('Fabric028', 88, 0, NULL, '/path/to/image28.jpg', 'running', NULL, NULL),
-  ('Fabric029', 102, 0, NULL, '/path/to/image29.jpg', 'running', NULL, NULL),
-  ('Fabric030', 115, 0, NULL, '/path/to/image30.jpg', 'running', NULL, NULL);
-
-
-
-INSERT INTO Defect_information (defect_code, detection_time, x_coordinate, y_coordinate, defect_type, image_path)
-VALUES
-  ('Defect001', '2023-11-14 08:45:00', 10, 15, 'Tear', '/path/to/defect_image1.jpg'),
-  ('Defect002', '2023-11-14 09:55:00', 20, 30, 'Color Fade', '/path/to/defect_image2.jpg'),
-  ('Defect003', '2023-11-14 10:45:00', 15, 25, 'Stitching Issue', '/path/to/defect_image3.jpg'),
-  ('Defect004', '2023-11-14 11:30:00', 5, 10, 'Hole', '/path/to/defect_image4.jpg'),
-  ('Defect005', '2023-11-14 12:15:00', 30, 40, 'Uneven Dye', '/path/to/defect_image5.jpg'),
-  ('Defect006', '2023-11-14 13:45:00', 12, 18, 'Tear', '/path/to/defect_image6.jpg'),
-  ('Defect007', '2023-11-14 14:30:00', 22, 32, 'Color Fade', '/path/to/defect_image7.jpg'),
-  ('Defect008', '2023-11-14 15:15:00', 17, 27, 'Stitching Issue', '/path/to/defect_image8.jpg'),
-  ('Defect009', '2023-11-14 16:15:00', 8, 12, 'Hole', '/path/to/defect_image9.jpg'),
-  ('Defect010', '2023-11-14 17:15:00', 25, 35, 'Uneven Dye', '/path/to/defect_image10.jpg'),
-  ('Defect011', '2023-11-14 18:30:00', 14, 22, 'Tear', '/path/to/defect_image11.jpg'),
-  ('Defect012', '2023-11-14 19:45:00', 28, 38, 'Color Fade', '/path/to/defect_image12.jpg'),
-  ('Defect013', '2023-11-14 20:30:00', 19, 29, 'Stitching Issue', '/path/to/defect_image13.jpg'),
-  ('Defect014', '2023-11-14 21:15:00', 7, 11, 'Hole', '/path/to/defect_image14.jpg'),
-  ('Defect015', '2023-11-14 22:30:00', 33, 45, 'Uneven Dye', '/path/to/defect_image15.jpg');
+    ('20231121140000-1', 1, '2023-11-21 14:05:00', 'hole', 10.0, 20.0, '/path/to/defect_image1.jpg'),
+    ('20231122101500-2', 2, '2023-11-22 10:20:00', 'stain', 15.0, 25.0, '/path/to/defect_image2.jpg'),
+    ('20231123103000-3', 3, '2023-11-23 10:35:00', 'puckering', 20.0, 30.0, '/path/to/defect_image3.jpg'),
+    ('20231124154500-4', 4, '2023-11-24 15:50:00', 'hole', 25.0, 35.0, '/path/to/defect_image4.jpg'),
+    ('20231125120000-5', 5, '2023-11-25 12:05:00', 'stain', 30.0, 40.0, '/path/to/defect_image5.jpg'),
+    ('20231126173000-6', 6, '2023-11-26 17:35:00', 'hole', 35.0, 45.0, '/path/to/defect_image6.jpg'),
+    ('20231127142000-7', 7, '2023-11-27 14:25:00', 'puckering', 40.0, 50.0, '/path/to/defect_image7.jpg'),
+    ('20231128111000-8', 8, '2023-11-28 11:15:00', 'hole', 45.0, 55.0, '/path/to/defect_image8.jpg'),
+    ('20231129164500-9', 9, '2023-11-29 16:50:00', 'stain', 50.0, 60.0, '/path/to/defect_image9.jpg'),
+    ('20231130132500-10', 10, '2023-11-30 13:30:00', 'hole', 55.0, 65.0, '/path/to/defect_image10.jpg');
