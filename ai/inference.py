5ec281618cacb805de57ed48bc2e9b9f33e293f8
Ju Hyunsahng <hsju2013@gmail.com>
2023-11-24 18:43:03 +0900

    add args

--- ai/inference.py
+++ ai/inference.py
@@ -1,26 +1,24 @@
 from ultralytics import YOLO
 import json
+import argparse
+import requests
 
 model = YOLO("./best.pt")
 
-result = model('./hole_bg_clean_1.jpg')
+# get result from arg
+parser = argparse.ArgumentParser()
+parser.add_argument("image_path", help="path to image")
+args = parser.parse_args()
+
+result = model(args.image_path)
 json_result = result[0].tojson()
 
-# Assuming 'json_results' contains your JSON string
 parsed_json = json.loads(json_result)
+
 
 # Pretty-print the JSON
 pretty_json = json.dumps(parsed_json, indent=2)
 print(pretty_json)
 
-# Optionally, you can write the pretty-printed JSON to a file
-with open('pretty_yolov8_results.json', 'w') as file:
-    file.write(pretty_json)
-
-# results = model(['./hole_bg_clean_1.jpg', './puckering_bg_clean_2.jpg', './stain_bg_clean_1_.jpg'])
-
-# for result in results:
-#     json_results = result.tojson()
-#     # save the results
-#     with open('results.json', 'w') as f:
-#         json.dump(json_results, f)
+# send result to server
+url = "http://localhost:3000/api/result"
